f9646dd15889051a86aa86686aff80273a87c809
meghana <meghanaeemani@gmail.com>
2025-12-26 11:42:07 -0500

    worked on java arthiOperators

---
+++ session-03-java-basics/java-basics-exercises/src/main/java/Operators/arithmeticOperators.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class arithmeticOperators {
+    public static void main(String[] args){
+        System.out.println("Integer Arithmetic");
+        int a = 1+2;
+        int b = a * 4;
+        int c = b / 5;
+        int d = c - a;
+        int e = -d;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+    }
+
+}
